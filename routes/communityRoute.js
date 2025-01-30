a5346e9c42ee39606933f6d63b7d1ccc92f73c93
Simon Beast <simonaseged75@gmail.com>
2025-01-30 19:56:45 +0300

    file upload fix

--- routes/communityRoute.js
+++ routes/communityRoute.js
@@ -7,5 +7,4 @@ const authGuard = require("../utils/AuthGuard");
 router.route('/').get(communityController.getAllCommunityBullets).post(authGuard.guard,authGuard.restrictTo("ADMIN"),imageUpload.single('picture'),communityController.createCommunityBullet);
 
 router.route('/:id').get(communityController.getCommunityBullet).patch(imageUpload.single('picture'),communityController.updateCommunityBullet).delete(communityController.deleteCommunityBullet);
-
-module.exports = router;
+module.exports = router;    
